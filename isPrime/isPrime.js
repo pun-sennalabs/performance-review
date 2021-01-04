bb1cedc6a49b7dd73a35658a1d369f94fcd7f850
Punn Sennalabs <pun@sennalabs.com>
2021-01-04 17:16:01 +0700

    feat: calculate prime

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -1,11 +1,15 @@
 export function isPrime(num) {
   if (num === 1) return false
+  if (num === 2) return true
   if (!num) throw new Error('Number is required')
-  if (parseInt(num) !== num || number <= 0)
+  if (parseInt(num) !== num || num <= 0)
     throw new Error('Number should be a positive integer')
   if (num > Number.MAX_SAFE_INTEGER)
     throw new Error('Number must less than or equal', Number.MAX_SAFE_INTEGER)
 
   const sqrtNum = Math.ceil(Math.sqrt(num))
-  throw new Error('Not implemented')
+  for (let i = 2; i <= sqrtNum; i++) {
+    if (num % i === 0) return false
+  }
+  return true
 }
