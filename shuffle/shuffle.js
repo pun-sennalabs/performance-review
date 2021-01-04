7e28ae7527a97f1b1454c435924cd90ead8567a2
Punn Sennalabs <pun@sennalabs.com>
2021-01-04 17:32:12 +0700

    feat: handle suffle cards

--- shuffle/shuffle.js
+++ shuffle/shuffle.js
@@ -3,7 +3,20 @@
  * @param {number[]} cards
  * @return {number[]}
  */
-export function shuffle(cards) {
-  throw new Error('Not implemented')
+export function shuffle(cards = []) {
+  let suffledCards = [...cards]
+  const n = cards.length
+  for (let i = 0; i < 100; i++) {
+    const shuffle = [getRandomInt(n), getRandomInt(n)].sort((a, b) => a - b)
+    suffledCards = [
+      ...suffledCards.slice(shuffle[0], shuffle[1] + 1),
+      ...suffledCards.slice(0, shuffle[0]),
+      ...suffledCards.slice(shuffle[1] + 1, n)
+    ]
+  }
+  return suffledCards
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
