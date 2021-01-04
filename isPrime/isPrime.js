334f14048bd18132e8a47f1c2808855aa05867cb
Punn Sennalabs <pun@sennalabs.com>
2021-01-04 17:12:32 +0700

    feat: throw error when input is more than max number [task-4]

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -3,5 +3,7 @@ export function isPrime(num) {
   if (!num) throw new Error('Number is required')
   if (parseInt(num) !== num || number <= 0)
     throw new Error('Number should be a positive integer')
+  if (num > Number.MAX_SAFE_INTEGER)
+    throw new Error('Number must less than or equal', Number.MAX_SAFE_INTEGER)
   throw new Error('Not implemented')
 }
