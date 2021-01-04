5effcb50a2de1c83d7bb8d2ede18f263301ad018
Punn Sennalabs <pun@sennalabs.com>
2021-01-04 17:10:48 +0700

    feat: throw error when input is a positive integer [task-3]

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -1,5 +1,7 @@
 export function isPrime(num) {
   if (num === 1) return false
   if (!num) throw new Error('Number is required')
+  if (parseInt(num) !== num || number <= 0)
+    throw new Error('Number should be a positive integer')
   throw new Error('Not implemented')
 }
