550de481397b6707e6b624edc184699a72fdf44f
Punn Sennalabs <pun@sennalabs.com>
2021-01-04 17:14:01 +0700

    feat: calculate sqrt of number [task-5]

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -5,5 +5,7 @@ export function isPrime(num) {
     throw new Error('Number should be a positive integer')
   if (num > Number.MAX_SAFE_INTEGER)
     throw new Error('Number must less than or equal', Number.MAX_SAFE_INTEGER)
+
+  const sqrtNum = Math.ceil(Math.sqrt(num))
   throw new Error('Not implemented')
 }
